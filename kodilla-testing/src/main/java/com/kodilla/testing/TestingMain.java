c0b68a384c35ba82ce89e641adc87cdedb7dfde2
binkul <56973040+binkul@users.noreply.github.com>
2019-11-06 14:57:18 +0100

    Task 6 2 (#2)
    * Adding new module
    
    * build.gradle modified for Error 'Gradle 6.0.
    
    * Task 6.2.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
+
+        // from submodul
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
         System.out.println("Pull Request first time");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK\n");
+        } else {
+            System.out.println("Error!\n");
+        }
+
+        // Task 6.2.
+        if (Calculator.add(340, 420) == 760) {
+            System.out.println("Method Calculator.add(int a, int b) is ok.");
+        } else {
+            System.out.println("Method Calculator.add(int a, int b) is wrong! Correct it !");
+        }
+
+        if (Calculator.subtract(700, 352) == 348) {
+            System.out.println("Method Calculator.subtract(int a, int b) is ok.");
+        } else {
+            System.out.println("Method Calculator.subtract(int a, int b) is wrong! Correct it!");
+        }
     }
 }
