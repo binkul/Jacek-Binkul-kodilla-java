5c5aef5a7eca6fa9319e96e8f37d403ac1e53754
binkul <jacekbinkul@gmail.com>
2019-11-07 15:13:56 +0100

    First test from modul 6.3.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,20 +6,6 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-        // from submodul
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-        System.out.println("Pull Request first time");
-
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK\n");
-        } else {
-            System.out.println("Error!\n");
-        }
-
         // Task 6.2.
         if (Calculator.add(340, 420) == 760) {
             System.out.println("Method Calculator.add(int a, int b) is ok.");
