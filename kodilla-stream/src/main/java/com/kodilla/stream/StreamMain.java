65613cf0ad12d54cc0d4f77168bd155654a336d2
binkul <jacekbinkul@gmail.com>
2019-11-12 21:56:31 +0100

    Task_7_1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,28 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text."));
 
-        System.out.println("Calculating expressions with lambdas");
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(4, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(40, 15, (a, b) -> a - b);
-        expressionExecutor.executeExpression(2, 8, (a, b) -> a * b);
-        expressionExecutor.executeExpression(120, 3, (a, b) -> a / b);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Jacek Binkul", text -> "### " + text + " ###");
+        poemBeautifier.beautify("Lato było przedziwne tego roku", text -> text.replaceAll(" ", "_"));
+        poemBeautifier.beautify("jacekbinkul", text -> text + "@gmail.com");
+        poemBeautifier.beautify("Jacek binkul", text -> text.toUpperCase());
+        poemBeautifier.beautify("Jacek     Binkul    pisze     programy", text -> text.replaceAll(" {2,}", " "));
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        PoemDecorator poemDecorator = (text) -> {
+            String result = "";
+            for (int i = text.length() - 1; i >= 0; i--) {
+                result += text.charAt(i);
+            }
+            return result;
+        };
+        System.out.println("Text from back: " + poemDecorator.decorate("Jacek Binkul"));
     }
 }
