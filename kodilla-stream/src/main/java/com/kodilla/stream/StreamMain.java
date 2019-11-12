aa8bfd50ecdf1a68641c672b18c9b06a2c8b4718
binkul <jacekbinkul@gmail.com>
2019-11-12 20:01:42 +0100

    Module 7.1. theory - lambdas and references

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        System.out.println("Calculating expressions with lambdas");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(4, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(40, 15, (a, b) -> a - b);
+        expressionExecutor.executeExpression(2, 8, (a, b) -> a * b);
+        expressionExecutor.executeExpression(120, 3, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    }
+}
