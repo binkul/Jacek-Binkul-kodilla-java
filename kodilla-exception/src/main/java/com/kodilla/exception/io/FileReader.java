ae3c24f48c70fd1d46bc8bcab3c6f249b1f1bac8
binkul <jacekbinkul@gmail.com>
2019-11-20 11:00:40 +0100

    Module 8.3. theory Throws Throw

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,17 +8,13 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
- //        Path path = Paths.get("D:/Jacek/Programy Intellij/Jacek-Binkul-kodilla-java/kodilla-exception/build/resources/main/file/names.txt");
-//        String path1 = file.getPath().replaceAll("%20", " ");
-//        Path path = Paths.get(file.getPath().replaceAll("%20", " "));
         try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath().replaceAll("%20", " ")))) {
-//            Stream<String> fileLines = Files.lines(path);
             fileLines.forEach(System.out::println);
         } catch (IOException ex) {
-            System.out.println("Oh no. Something is wrong !!!" + ex);
+            throw new FileReaderException();
         } finally {
             System.out.println("I will do it always ...");
         }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,33 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+import java.io.IOException;
 
 public class ExceptionModuleRunner {
     public static void main(String[] args) {
+
+        FileReader fileReader = new FileReader();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException ex) {
+            System.out.println("Problem with reading file ...");
+        }
+/*
+        // In FileReaderWithoutHandling we move (via throws IOException) Exception to class, which is using FileReaderWithoutHandling.readFile()
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+        try {
+            fileReaderWithoutHandling.readFile();
+        } catch (IOException ex) {
+            System.out.println("File Error");
+        }
+*/
+/*
         FileReader fileReader = new FileReader();
         fileReader.readFile();
+*/
     }
 
 }
