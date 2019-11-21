f2bdc8ce99c0c9cde4334496a804c62c3303ec7a
binkul <56973040+binkul@users.noreply.github.com>
2019-11-21 10:03:14 +0100

    Task_8_2 (#14)
    * Task_8_2
    
    * Taks_8_2 improve

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,38 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        boolean errorOccur = false;
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException ex) {
+            System.out.println("Divide by zero detected. You have to change parameters.");
+            //ex.printStackTrace(); - this optional. Up is description about Exception, we dont need StackTree!
+            errorOccur = true;
+        } finally {
+            if (errorOccur) {
+                System.out.println("The result is unspecified.");
+            } else {
+                System.out.println("The result is correct.");
+            }
+        }
+
+
+    }
+}
