be1f5c40d501d84c9b3707fe0bf501b7dbfe9328
binkul <jacekbinkul@gmail.com>
2020-01-01 13:16:59 +0100

    Task_13_1

--- kodilla-good-patterns/src/main/java/patternsMain.java
+++ kodilla-good-patterns/src/main/java/patternsMain.java
@@ -1,6 +1,10 @@
+import com.kodilla.good.patterns.challenges.MovieStore;
+
 public class patternsMain {
 
     public static void main(String[] args) {
 
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore);
     }
 }
