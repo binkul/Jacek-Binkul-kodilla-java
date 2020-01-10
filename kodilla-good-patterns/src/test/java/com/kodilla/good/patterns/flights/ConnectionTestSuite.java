dc9673987905f9b9ba4ed675d1554b572cf4eb29
binkul <jacekbinkul@gmail.com>
2020-01-10 22:00:34 +0100

    Test Good patterns improve

--- kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/flights/ConnectionTestSuite.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/flights/ConnectionTestSuite.java
@@ -30,86 +30,53 @@ public class ConnectionTestSuite {
         //When - Attention! amount can change during town and connection update
         System.out.println("Test for collection size ...");
         int airportsCount = domesticConnection.getAirports().size();
-        int connectionCount = domesticConnection.getFlights().size();
 
         //Then
         Assert.assertEquals(6, airportsCount);
-        Assert.assertEquals(23, connectionCount);
     }
 
     @Test
     public void fromFindConnectionList() {
 
         //Given
-        String[] CONNECTION = {
-                "WAW001|POZNAN|BYDGOSZCZ",
-                "WAW002|KRAKOW|POZNAN",
-                "WAW003|WARSZAWA|KRAKOW",
-                "WAW004|GDANSK|LODZ",
-                "WAW005|WARSZAWA|BERLIN",
-                "WAW006|WARSZAWA|GDANSK"
-        };
         Connectable connection = new Connection();
-        Set<Flight> flights = connection.createFlights(CONNECTION);
-        Flight flight = new Flight("WAW001", "Poznań", "Bydgoszcz");
+        Flight flight = new Flight("WAW001", "Warszawa", "Bydgoszcz");
 
         //When
         System.out.println("Test connection from ...");
-        Set<Flight> fromPoznan = connection.findFlightFrom(flights, "Poznań");
+        Set<Flight> fromWarszawa = connection.findFlightFrom("Warszawa");
 
         //Then
-        Assert.assertEquals(flight, fromPoznan.iterator().next());
+        Assert.assertTrue(fromWarszawa.contains(flight));
     }
 
     @Test
     public void toFindConnectionList() {
 
         //Given
-        String[] CONNECTION = {
-                "WAW001|POZNAN|BYDGOSZCZ",
-                "WAW002|KRAKOW|POZNAN",
-                "WAW003|WARSZAWA|KRAKOW",
-                "WAW004|GDANSK|LODZ",
-                "WAW005|WARSZAWA|BERLIN",
-                "WAW006|WARSZAWA|GDANSK"
-        };
         Connectable connection = new Connection();
-        Set<Flight> flights = connection.createFlights(CONNECTION);
-        Flight flight = new Flight("WAW002", "Kraków", "Poznań");
+        Flight flight = new Flight("KRK003", "Kraków", "Poznań");
 
         //When
         System.out.println("Test connection to ...");
-        Set<Flight> toPoznan = connection.findFlightTo(flights, "Poznań");
+        Set<Flight> toPoznan = connection.findFlightTo("Poznań");
 
         //Then
-        Assert.assertEquals(1, toPoznan.size());
-        Assert.assertEquals(flight, toPoznan.iterator().next());
+        Assert.assertEquals(3, toPoznan.size());
+        Assert.assertTrue(toPoznan.contains(flight));
     }
 
     @Test
     public void viaFindConnectionList() {
 
         //Given
-        String[] CONNECTION = {
-                "WAW001|POZNAN|BYDGOSZCZ",
-                "WAW002|BYDGOSZCZ|WARSZAWA",
-                "WAW003|WARSZAWA|KRAKOW",
-                "WAW004|GDANSK|LODZ",
-                "WAW005|WARSZAWA|BERLIN",
-                "WAW006|WARSZAWA|GDANSK"
-        };
         Connectable connection = new Connection();
-        Set<Flight> flights = connection.createFlights(CONNECTION);
-        Flight destination = new Flight("WAW002", "Bydgoszcz", "Warszawa");
-        Flight arrival = new Flight("WAW005", "Warszawa", "Berlin");
 
         //When
         System.out.println("Test connection via ...");
-        Set<FlightVia> toPoznan = connection.findFlightVia(flights, "Bydgoszcz", "Berlin");
+        Set<FlightVia> toPoznan = connection.findFlightVia("Bydgoszcz", "Berlin");
 
         //Then
-        Assert.assertEquals(1, toPoznan.size());
-        Assert.assertEquals(destination, toPoznan.iterator().next().getFlightFrom());
-        Assert.assertEquals(arrival, toPoznan.iterator().next().getFlightTo());
+        Assert.assertEquals(2, toPoznan.size());
     }
 }
