4323e25b98a80ed2c6f361b4d101c2b933e75df0
binkul <56973040+binkul@users.noreply.github.com>
2020-01-28 18:48:09 +0100

    Task 16_6 (#30)

--- kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
@@ -2,6 +2,10 @@ package com.kodilla.patterns;
 
 public class PatternsRunner {
     public static void main(String[] args){
-        System.out.println("Welcome to the module 11 - Patterns");
+
+        Double a = 0.3;
+        Integer b = 8;
+
+        System.out.println("Welcome to the module 11 - Patterns " + (a + b));
     }
 }
