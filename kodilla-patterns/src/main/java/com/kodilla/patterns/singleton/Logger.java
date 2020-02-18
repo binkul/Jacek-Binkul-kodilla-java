a53b7d4323daf8959c5c096c0f0bc4728f934cda
binkul <jacekbinkul@gmail.com>
2020-02-18 09:00:58 +0100

    Singleton modyfication

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -1,6 +1,27 @@
 package com.kodilla.patterns.singleton;
 
 public class Logger {
+    private String lastLog = "";
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final Logger INSTANCE = new Logger();
+    }
+
+    public void log(String log) {
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
+/*
     private static Logger loggerInstance = null;
     private String lastLog = "";
 
@@ -26,4 +47,5 @@ public class Logger {
     public String getLastLog() {
         return lastLog;
     }
+*/
 }
