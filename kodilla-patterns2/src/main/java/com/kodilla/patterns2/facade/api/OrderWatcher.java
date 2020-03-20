4657b5a75bd9f8230c5eeef6cb503dfe0fb93292
binkul <56973040+binkul@users.noreply.github.com>
2020-03-20 09:01:37 +0100

    Task_24_3 (#38)

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
@@ -0,0 +1,43 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class OrderWatcher {
+    private final static Logger LOGGER = LoggerFactory.getLogger(OrderWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, orderId) && target(object)")
+    public void logEvent(OrderDto order, Long orderId, Object object) {
+        LOGGER.info("Logging the order event" + object.getClass().getName() +
+                "; \norder ID:" + orderId +
+                "; \nordered items: \n" + order.getItems().stream()
+                .map(i -> {return "product ID:" + i.getProductId() + ", quantity: " + i.getQuantity();})
+                .collect(Collectors.joining("\n")));
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        Object result;
+
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+}
