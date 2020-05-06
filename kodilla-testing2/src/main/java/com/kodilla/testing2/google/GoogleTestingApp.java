46b77009d289ec3c5a67b01fa921169d2cc783b2
binkul <jacekbinkul@gmail.com>
2020-05-06 21:15:23 +0200

    Improve error with chromedriver version

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -6,13 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "gLFyf";
+    public static final String SEARCHFIELD = "q";
 
     public static void main(String[] args) {
-        WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         webDriver.get("https://www.google.com");
 
-        WebElement searchField = webDriver.findElement(By.className(SEARCHFIELD));
+        WebElement searchField = webDriver.findElement(By.name(SEARCHFIELD));
+//        Actions actions = new Actions(webDriver);
+//        actions.moveToElement(searchField).click();
+//        actions.sendKeys("Kodilla");
+//        actions.perform();
+
         searchField.sendKeys("Kodilla");
         searchField.submit();
     }
